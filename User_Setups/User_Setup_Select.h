3d07f9cf429096f18d8b0ba3f8676acaa7ea0e22
MOFOGH <37545640+MOFOGH@users.noreply.github.com>
2025-03-24 01:58:41 +0100

    Update User_Setup_Select.h
    For Lilygo T-display ESP32-S3 Touch

--- User_Setups/User_Setup_Select.h
+++ User_Setups/User_Setup_Select.h
@@ -41,8 +41,8 @@
 //#include <User_Setup_m5cardputer.h>              // (For M5Cardputer)
 //----------------------------------------------------------------------------------------
 
-#include <User_Setup_TTGO_NoTouch.h>             // (For TTGO T-Display)
-//#include <User_Setup_TTGO_Touch.h>               // (For TTGO T-Display Touch)
+//#include <User_Setup_TTGO_NoTouch.h>             // (For TTGO T-Display)
+#include <User_Setup_TTGO_Touch.h>               // (For TTGO T-Display Touch)
 //#include <User_Setup_TTGO_marauder.h>            // (For Marauder TTGO T-Display)
 //#include <User_Setup_LilyGo_T_Embed_S3.h>        // (For T-Embed Display)
 //----------------------------------------------------------------------------------------
@@ -161,7 +161,7 @@
 
 //#include <User_Setups/Setup205_ESP32_TouchDown_S3.h>     // Setup file for the ESP32 TouchDown S3 based on ILI9488 480 x 320 TFT 
 
-//#include <User_Setups/Setup206_LilyGo_T_Display_S3.h>     // For the LilyGo T-Display S3 based ESP32S3 with ST7789 170 x 320 TFT
+#include <User_Setups/Setup206_LilyGo_T_Display_S3.h>     // For the LilyGo T-Display S3 based ESP32S3 with ST7789 170 x 320 TFT
 //#include <User_Setups/Setup207_LilyGo_T_HMI.h>            // For the LilyGo T-HMI S3 based ESP32S3 with ST7789 240 x 320 TFT
 //#include <User_Setups/Setup209_LilyGo_T_Dongle_S3.h>      // For the LilyGo T-Dongle S3 based ESP32 with ST7735 80 x 160 TFT
 //#include <User_Setups/Setup210_LilyGo_T_Embed_S3.h>         // For the LilyGo T-Embed S3 based ESP32S3 with ST7789 170 x 320 TFT
